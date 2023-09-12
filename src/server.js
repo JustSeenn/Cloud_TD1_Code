2f958ce5427b6e177d512b629ff346856730f244
I576958 <vincent.boillot.devalliere@sap.com>
2023-09-12 16:05:50 +0200

    feat: the code

---
+++ src/server.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(express.json());
+
+const dbConfig = {
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+};
+
+const pool = new Pool(dbConfig);
+const port = 8080;
+
+pool
+  .connect()
+  .then(() => {
+    console.log("Connexion à la base de données établie.");
+  })
+  .catch((err) => {
+    console.error("Erreur de connexion à la base de données : " + err);
+  });
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/data", async (req, res) => {
+  try {
+    // Exécutez une requête SQL pour sélectionner toutes les données dans la table
+    const query = "SELECT * FROM quote";
+
+    const result = await pool.query(query);
+
+    res.json(result.rows);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Erreur lors de la récupération des données : " + err });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
+});
